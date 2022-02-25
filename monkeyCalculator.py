5472bbf15fb715fda49ec2d1435991624d182fc9
Abbas Dargahi <adargahi97@gmail.com>
2022-02-25 10:35:42 -0600

    Assignment 4.5.2

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -4,8 +4,14 @@ def calculateTime():
     monkey_one = input("Is the first monkey smiling?:  ")
     monkey_two = input("Is the second monkey smiling?: ")
 
-    # end assignment
+    if monkey_one.lower() == "y" and monkey_two.lower() == "y":
+        print("Uh Oh! We're in trouble!")
+    elif monkey_one.lower() == "n" and monkey_two.lower() == "n":
+        print("Uh Oh! We're in trouble!")
+    else:
+        print("Yay! We're going to have a good day!")
 
+    # end assignment
 ## if you want to test locally before you try to sync
 ## uncomment calculateTime() and run > python monkeyCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
